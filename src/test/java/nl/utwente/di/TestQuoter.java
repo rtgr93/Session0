4076dc44df64ac2b6f424856c7941b43554abb9b
rtgr93 <r.d.deolde@student.utwente.nl>
2015-04-20 16:50:45 +0200

    pom

---
+++ src/test/java/nl/utwente/di/TestQuoter.java
@@ -0,0 +1,12 @@
+package nl.utwente.di;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestQuoter {
+	public void testBook1() throws Exception {
+		Quoter quoter = new Quoter();
+		double price = quoter.getBookPrice("1");
+		Assert.assertEquals("Price of book 1", 10.0, price, 0.0);
+	}
+}
